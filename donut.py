e8d01c62a5acb2b5ccde8b5eb6d6330a5ebb053f
Tabarakul Islam <itabarakul@gmail.com>
2022-11-05 00:18:16 +0530

    fixed spacing

--- donut.py
+++ donut.py
@@ -21,16 +21,16 @@ R1, R2 = 100, 200
 def circleNates(radius, center=[R2, 0, 0], spacing=10):
     posCircle = []
     for theta in range(0, 629, spacing):
-        xyz = np.add(center, [radius*np.cos(theta), radius*np.sin(theta), 0])
+        xyz = np.add(center, [radius*np.cos(theta/100), radius*np.sin(theta/100), 1])
         posCircle.append(xyz)
     return posCircle
 
 def torusNates(positions, spacing=10):
     posDonut = []
-    for phi in range(0, 314, spacing):
-        xyz = [positions[0]*np.cos(phi), 
+    for phi in range(0, 2*314, spacing):
+        xyz = [positions[0]*np.cos(phi/100), 
                positions[1], 
-               -positions[0]*np.sin(phi)]
+               -positions[0]*np.sin(phi/100)]
         posDonut.append(xyz)
     return posDonut
 
@@ -49,8 +49,7 @@ while run:
         # pygame.draw.circle(screen, (250, 250, 250), (nate[0]+x_offset, nate[1]+y_offset), 2)
         for j in torusNates(i):
             dnate = j[:-1]
-            if j[2]>0:
-                pygame.draw.circle(screen, color, (dnate[0]+x_offset, dnate[1]+y_offset), 1)
+            pygame.draw.circle(screen, color, (dnate[0]+x_offset, dnate[1]+y_offset), 1)
             # else: pygame.draw.circle(screen, (200, 200, 0), (dnate[0]+x_offset, dnate[1]+y_offset), 1)
     pygame.display.update()
 
