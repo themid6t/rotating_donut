2f7ae77a7d44e8f19ee3780a825fa0179ad27875
Tabarakul Islam <itabarakul@gmail.com>
2022-11-08 23:03:20 +0530

    rotation done, zbuffer left and calculating normals later

--- donut.py
+++ donut.py
@@ -1,86 +1,57 @@
 import pygame
 import numpy as np
 import math
+import random
 
 pygame.init()
 
 WIDTH = 800
 HEIGHT = 800
-FPS = 30
+FPS = 60
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 rows = WIDTH
 columns = HEIGHT
 screen_size = rows*columns
-alpha = 10
-color = (200, 200, 200, alpha)
 x_offset = rows / 2
 y_offset = columns / 2
 
-A, B = 10, 10
+A, B = -0.008, 0.004  #A --> X rotation angle, B --> Y rotation angle
 R1, R2 = 100, 200
-spacing = 40 #21
+spacing = 10 #21
+radius = 2
 
-def circleNates(radius, center=[R2, 0, 0], spacing=10):
-    posCircle = []
-    for theta in range(0, 629, spacing):
-        xyz = np.add(center, [radius*np.cos(theta/100), radius*np.sin(theta/100), 0])
-        posCircle.append(xyz)
-    return posCircle
-
-def torusNates(positions, spacing=10):
-    posDonut = []
-    for phi in range(0, 2*314, spacing):
-        xyz = [positions[0]*np.cos(phi/100), 
-               positions[1], 
-               -positions[0]*np.sin(phi/100)]
-        posDonut.append(xyz)
-    return posDonut
-
-def torus(R1, R2, center=[0, 0, 0], spacing=10):
+def all_positions(R1, R2, spacing=10):
     positions = []
     for theta in range(0, 629, spacing):
         for phi in range(0, 629, spacing):
-            xyz = [(R2+R1*np.cos(theta/100))*np.cos(phi/100),
-                    R1*np.sin(theta/100), 
-                    -(R2+R1*np.cos(theta/100)*np.sin(phi/100))]
-            positions.append(xyz)
+            x, y, z = (R2+R1*np.cos(theta/100))*np.cos(phi/100), R1*np.sin(theta/100), -(R2+R1*np.cos(theta/100))*np.sin(phi/100)
+            positions.append([x, y, z])
     return positions
 
-def zBuffer(R1, R2, zPosition, luminance=[50, 200]):
-    divs = int((luminance[1]-luminance[0])/(R1+R2)*100)
-    lumins = [i/100 for i in range(luminance[0]*100, luminance[1]*100, divs)]
-    weight = zPosition
-    if weight >= 0: return int(lumins[math.floor(weight)])
-    else: return int(lumins[::-1][math.floor(abs(weight))])
+def zBuffer(R1, R2, zPos):
+    alpha = 255
+    return alpha
 
-def drawCircle(positions):
+def drawCircle(positions, radius=2):
     for i in positions:
-        # alpha = zBuffer(R1, R2, i[-1])
-        color = (200, 200, 200) #(alpha, alpha, alpha)
-        pygame.draw.circle(screen, color, (i[0]+x_offset, i[1]+y_offset), 2)
+        alpha = zBuffer(R1, R2, i[-1])
+        color = (alpha, alpha, alpha)
+        pygame.draw.circle(screen, color, (i[0]+x_offset, i[1]+y_offset), radius)
 
 
 def rotate(positions, A, B):
     X = ([1, 0, 0], [0, np.cos(A), np.sin(A)], [0, -np.sin(A), np.cos(A)])
     Y = ([np.cos(B), np.sin(B), 0], [-np.sin(B), np.cos(B), 0], [0, 0, 1])
-    rotate = np.dot(X, Y)
+    rotation = np.dot(X, Y)
     for i in range(len(positions)):
-        positions[i] = np.dot(i, rotate)
+        positions[i] = np.dot(positions[i], rotation)
     
     return positions
     
-# print(don[0])
-# cir = circleNates(R1, spacing=spacing)
-# don = [torusNates(i) for i in cir]
-# print(len(cir), len(don), len(don[0]))
-# l = [i[-1] for i in don]
-# print(zBuffer(R1, R2, min(l)))
-# print(min(l), max(l), len(l))
-cir = torus(R1, R2)
-# ls = rotate(cir, A, B)
-# print(ls[3])
+cir = all_positions(R1, R2, spacing)
+
 i = 0
 run = True
 
@@ -92,10 +63,10 @@ while run:
     # if i > 15:
     #     i=0
     # pos = don[i]
-    # pygame.time.wait(100)
-    drawCircle(cir)
+    # pygame.time.wait(200)
+    drawCircle(cir, radius)
 
-    # cir = rotate(cir, A, B)
+    cir = rotate(cir, A, B)
 
 
     pygame.display.update()
